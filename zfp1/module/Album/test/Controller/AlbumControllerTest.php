62ab69a5f724b3411b1b18d51575c2e09746aa10
Massutatsu Inumaru <massutatsumhi@gmail.com>
2017-05-24 15:19:54 -0300

    Configuração do ServiceManager para testes

--- zfp1/module/Album/test/Controller/AlbumControllerTest.php
+++ zfp1/module/Album/test/Controller/AlbumControllerTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace AlbumTest\Controller;
 
+use Album\Model\AlbumTable;
+use Zend\ServiceManager\ServiceManager;
 use Album\Controller\AlbumController;
 use Zend\Stdlib\ArrayUtils;
 use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
@@ -8,6 +10,7 @@ use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
 class AlbumControllerTest extends AbstractHttpControllerTestCase
 {
     protected $traceError = true;
+    protected $albumTable;
 
     public function setUp()
     {
@@ -18,22 +21,19 @@ class AlbumControllerTest extends AbstractHttpControllerTestCase
         $configOverrides = [];
 
         $this->setApplicationConfig(ArrayUtils::merge(
-            // Grabbing the full application configuration:
             include __DIR__ . '/../../../../config/application.config.php',
             $configOverrides
-        ));
+            ));
+
         parent::setUp();
 
-        $services = $this->getApplicationServiceLocator();
-        $config = $services->get('config');
-        unset($config['db']);
-        $services->setAllowOverride(true);
-        $services->setService('config', $config);
-        $services->setAllowOverride(false);
+        $this->configureServiceManager($this->getApplicationServiceLocator());
     }
 
     public function testIndexActionCanBeAccessed()
     {
+        $this->albumTable->fetchAll()->willReturn([]);
+
         $this->dispatch('/album');
         $this->assertResponseStatusCode(200);
         $this->assertModuleName('Album');
@@ -41,4 +41,27 @@ class AlbumControllerTest extends AbstractHttpControllerTestCase
         $this->assertControllerClass('AlbumController');
         $this->assertMatchedRouteName('album');
     }
+
+    protected function configureServiceManager(ServiceManager $services)
+    {
+        $services->setAllowOverride(true);
+
+        $services->setService('config', $this->updateConfig($services->get('config')));
+        $services->setService(AlbumTable::class, $this->mockAlbumTable()->reveal());
+
+        $services->setAllowOverride(false);
+    }
+
+    protected function updateConfig($config)
+    {
+        $config['db'] = [];
+        return $config;
+    }
+
+    protected function mockAlbumTable()
+    {
+        $this->albumTable = $this->prophesize(AlbumTable::class);
+        return $this->albumTable;
+    }
+
 }
